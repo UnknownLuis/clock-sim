a47c6fae1c702a52b77d31061c9ceb81da4dc569
uriapete <uriarteangelo@gmail.com>
2025-11-25 10:48:39 -0800

    CT - javadoc top

--- src/clockSim/ClockTime.java
+++ src/clockSim/ClockTime.java
@@ -4,7 +4,9 @@
 package clockSim;
 
 /**
- * 
+ * @author Peter Uriarte
+ * @version 0.1.0.0 (Epoch SemVer)
+ * Data Type that handles conversion between Unix integer and Hours:Minutes:Seconds.
  */
 public class ClockTime {
 
