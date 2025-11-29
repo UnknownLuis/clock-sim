3703a0d5af45c355b375cb99b41e69f6c6f4c02f
uriapete <uriarteangelo@gmail.com>
2025-11-29 14:55:46 -0800

    src/clockSim/NaturalClock.java - update jdoc

--- src/clockSim/NaturalClock.java
+++ src/clockSim/NaturalClock.java
@@ -2,9 +2,9 @@ package clockSim;
 
 /**
  * Mid level class which represents set of digital clocks.
- * @author Luis Martinez
- * @version 0.1.0.0 (MARKETING.BREAKING.FEATURE.PATCH) - 
- * Create class
+ * @author Luis Martinez & Peter Uriarte
+ * @version 0.1.1.0 (MARKETING.BREAKING.FEATURE.PATCH) - 
+ * Extends Clock, new constructor
  */
 public abstract class NaturalClock extends Clock {
 
