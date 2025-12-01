4950671688978b6fe1f7923f8ba7dbe9166c4bdf
uriapete <uriarteangelo@gmail.com>
2025-11-30 19:40:25 -0800

    src/clockSim/AtomicClock.java - add jdoc

--- src/clockSim/AtomicClock.java
+++ src/clockSim/AtomicClock.java
@@ -1,5 +1,10 @@
 package clockSim;
 
+/**
+ * Class that represents an Atomic Clock. (Zoom! You can't see me!)
+ * @author Luis Martinez
+ * @version 0.1.0.0 (marketing.BREAKING.Features.patch) - Create class
+ */
 public class AtomicClock extends QuantumClock {
 
 }
